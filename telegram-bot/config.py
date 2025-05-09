515758f5cbf0f86dffb847fd043e6c5c185399f1
Trần Công Minh <64911983+minhtranc1991@users.noreply.github.com>
2025-05-09 10:16:25 +0700

    Add configuration and functions for Binance trading bot; implement balance reporting and message sending

---
+++ telegram-bot/config.py
@@ -0,0 +1,16 @@
+acc_binance_1 = {
+'API_KEY': 'ZRiebGRJJkESaO3W4S4l6mKhCQRnsD4jUnogMZ4w4Zxq2K4b2cOkZaJhPCmnl5ql',
+'API_SECRET': 'V7sIOFLzvZM0A4jaRBF04flTD6x8gPYpzKBsHDE0vzAbPB3z06MvjomczpDRBomU'
+
+}
+acc_testnet = {
+'API_KEY': '98fa963c29d068f67d65ce705ce51749eb5a9fb4a86ea9715cd5348fc40815ba',
+'API_SECRET': 'a167025700ec25344834d2553b45f4f8c5a1d2c748c762b3f0c93034ad6acaad'
+}
+# token = '7562488970:AAGztkJbrXlwhkLtwKyvqB-WSTs8EkbL8JE'
+# bot_id = '6681378248'
+# channel_strat_id = '-1002365511251'
+
+token = '7799356461:AAHb4YMXqmahG-l6wM4AAMX8n0rQqXngOeo'
+bot_id = '1127911996'
+channel_strat_id = '-1002669742910'
